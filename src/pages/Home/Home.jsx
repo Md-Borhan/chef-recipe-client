00fda38bfbe228602bff5390c3f6e95d301b0de5
Borhan Uddin <borhanm373@gmail.com>
2023-05-01 23:18:09 +0600

    footer component done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import NavigationBar from "../share/NavigationBar";
 import { Outlet } from "react-router-dom";
+import Footer from "../share/footer";
 
 const Home = () => {
   return (
     <div>
       <NavigationBar></NavigationBar>
-      <Outlet></Outlet>
+      <div className="h-[calc(100vh-184px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
---
+++ src/pages/share/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="py-7 bg-slate-900">
+      <p className="text-white text-center">
+        &copy; Kruton 2023 | Created with by{" "}
+        <span className="text-[#FB834A]">Borhan</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
